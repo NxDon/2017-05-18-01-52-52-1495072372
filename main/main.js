40ecf153c50a7129339c5069f25051ecde6c7ad0
NxDon <nxduanyu@gmail.com>
2017-07-26 23:43:30 +0800

    finish request

--- main/main.js
+++ main/main.js
@@ -1,18 +1,35 @@
 module.exports = function main() {
     let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
     console.log(`o) 最小值 = ${sequence.minimum()}
-// Write your code here
-`);
+o) 最大值 = ${sequence.maxmum()}
+o) 元素数量 = ${sequence.elemsCount()}
+o) 平均值 = ${sequence.getAverage()}`);
 };
 
 class Sequence {
   constructor(input) {
     // Write your code here
+    this.value = input;
+    this.sortedArr = this.value.sort(function(a,b){
+  		return a - b;
+  	})
   }
 
   minimum() {
+  	
+  	return this.sortedArr[0]
     // Write your code here
   }
+  maxmum(){
+  	return this.sortedArr[this.sortedArr.length - 1];
+  }
+  elemsCount(){
+  	return this.value.length;
 
+  }
+  getAverage(){
+  	var sum = this.value.reduce(function(a,b){return a + b});
+  	return (sum / this.elemsCount()).toFixed(2);
+  }
   // Write your code here
 }
